60d68a7e7a0080b1987259434a50dadee30a880a
GalerDekstop <gabigaler50@gmail.com>
2022-06-25 20:12:09 +0300

    Add logic for menu selection page when adding and removing items

---
+++ assets/mocked_foods.js
@@ -0,0 +1,18 @@
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
+export default {
+  restaurant: [
+    {
+      name: "MC",
+      products: [
+        {
+          name: "Big mc",
+          price: 13.13
+        },
+        {
+          name: "mc toast",
+          price: 13.13
+        }
+      ]
+    }
+  ]
+};
--- pages/food.tsx
+++ pages/food.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Link from "next/link";
 
-const Food: NextPage = ({ foodList, groupSize = 1 }) => {
+const Food: NextPage = () => {
   useEffect(() => {
     // Update the document title using the browser API
     navigator.geolocation.getCurrentPosition(function (position) {
@@ -12,11 +13,71 @@ const Food: NextPage = ({ foodList, groupSize = 1 }) => {
     });
   }, []);
 
+  const foodCarousel = <div>food selector</div>;
+  const [foodSelectors, setFoodSelectors] = useState([foodCarousel]);
+
+  const users = [
+    { name: "you" },
+    {
+      name: "your friend",
+    },
+  ];
+
   return (
     <div>
-      {Array.from({ length: groupSize }, (v, i) => i).map((index) => (
-        <div key={index}></div>
-      ))}
+      {/* HEADER */}
+      <div className="bg-black w-full flex justify-between">
+        <div className="text-white p-8 px-10 text-xl font-bold">WEAT</div>
+        <div className="text-white p-8 px-10 text-xl font-bold">
+          <Link href={"/"}>
+            <a>INVITE</a>
+          </Link>
+        </div>
+      </div>
+
+      {/* FLEX WITH YOUR FRIENDS */}
+      <div className="flex gap-2 justify-evenly">
+        {users.map(({ name }, index) => (
+          <div
+            className={`m-8 ${name === "you" && "border-red-600 border-2"}`}
+            key={index}
+          >
+            <div className="m-4">{name}</div>
+            <div>restaurant selector</div>
+            {foodSelectors.map((elem, index) => {
+              if (index > 0 && name === "you") {
+                return (
+                  <div className="flex justify-center">
+                    {elem}
+                    <button
+                      className="rounded-full bg-black text-white w-8 h-8"
+                      onClick={() => {
+                        const temp = foodSelectors.slice(0, -1);
+                        setFoodSelectors(temp);
+                      }}
+                    >
+                      -
+                    </button>
+                  </div>
+                );
+              }
+              return elem;
+            })}
+            {name === "you" && (
+              <button
+                className="rounded-full bg-black text-white w-8 h-8"
+                onClick={() =>
+                  setFoodSelectors([...foodSelectors, foodCarousel])
+                }
+              >
+                +
+              </button>
+            )}
+            <hr className="bg-black h-1" />
+            <div>total: </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
