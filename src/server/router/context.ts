b14b620bf4000fb3f2fe7e16687faa9f6de6ee95
galer7 <gabigaler50@gmail.com>
2022-07-31 17:39:03 +0300

    Change credentials login with Google OAuth login

--- src/server/router/context.ts
+++ src/server/router/context.ts
@@ -2,7 +2,7 @@
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 import { unstable_getServerSession as getServerSession } from "next-auth/next";
-import { makeAuthOptions as makeNextAuthOptions } from "@/pages/api/oauth/[...nextauth]";
+import { makeAuthOptions as makeNextAuthOptions } from "@/pages/api/auth/[...nextauth]";
 import { prisma } from "@/server/db/client";
 
 export const createContext = async (
